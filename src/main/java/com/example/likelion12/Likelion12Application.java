19cb58b742d32cf6598317f8e253e09dc05f76b7
jungeun <kje2066@naver.com>
2024-07-18 16:38:53 +0900

    init commit

---
+++ src/main/java/com/example/likelion12/Likelion12Application.java
@@ -0,0 +1,13 @@
+package com.example.likelion12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion12Application.class, args);
+	}
+
+}
