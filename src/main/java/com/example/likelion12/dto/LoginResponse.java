7d2817707509987d713d085ebdc8a4efe4344c81
jungeun <kje2066@naver.com>
2024-07-31 11:13:51 +0900

    feat : LoginResponse dto 작성

--- src/main/java/com/example/likelion12/dto/LoginResponse.java
+++ src/main/java/com/example/likelion12/dto/LoginResponse.java
@@ -1,10 +1,16 @@
 package com.example.likelion12.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class LoginResponse {
     /**
      * 카카오 소셜 로그인
      */
-    private long memberId;
+    private boolean memberStatus;
+    private Long memberId;
     private String profileImage;
     private String nickname;
     private String accessToken;
