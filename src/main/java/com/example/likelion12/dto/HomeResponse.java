9bf245450caa319b3f92673ee16ba81c8e95c988
손명진 <sonmyeongjin@sonmyeongjin-ui-MacBookAir.local>
2024-08-01 02:11:54 +0900

    feat : DTO 변경 및 레퍼지토리, 서비스 추가

--- src/main/java/com/example/likelion12/dto/HomeResponse.java
+++ src/main/java/com/example/likelion12/dto/HomeResponse.java
@@ -1,2 +1,12 @@
-package com.example.likelion12.dto;public class HomeResponse {
+package com.example.likelion12.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class HomeResponse {
+    private List<Socialring> deadline_imminent;
+    private List<Crew> hot_crew;
 }
--- src/main/java/com/example/likelion12/dto/Socialring.java
+++ src/main/java/com/example/likelion12/dto/Socialring.java
@@ -1,2 +1,16 @@
-package com.example.likelion12.dto;public class Socialring {
+package com.example.likelion12.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Socialring {
+    private String socialring_name;
+    private String socialring_img;
+    private String socialring_date;
+    private Long active_area_id;
+    private Integer total_recruits;
+    private Integer socialring_cost;
+    private String comment_simple;
 }
