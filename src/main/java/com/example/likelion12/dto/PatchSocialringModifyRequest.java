508d3b397e77286b2e75f7caad240b4be8589739
hd0rable <kanghuijin12@gmail.com>
2024-08-01 23:41:16 +0900

    feat : PatchSocialringModifyRequest ado 작성

--- src/main/java/com/example/likelion12/dto/PatchSocialringModifyRequest.java
+++ src/main/java/com/example/likelion12/dto/PatchSocialringModifyRequest.java
@@ -2,18 +2,22 @@ package com.example.likelion12.dto;
 
 import com.example.likelion12.domain.base.BaseGender;
 import com.example.likelion12.domain.base.BaseLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
+@Getter
+@AllArgsConstructor
 public class PatchSocialringModifyRequest {
     private String socialringName;
     private String socialringImg;
-    private long activityRegionId;
-    private long facilityId;
-    private long exerciseId;
-    private int totalRecruits;
+    private Long activityRegionId;
+    private Long facilityId;
+    private Long exerciseId;
+    private Integer totalRecruits;
     private LocalDate socialringDate;
-    private int  socialringCost;
+    private Integer  socialringCost;
     private String comment;
     private String commentSimple;
     private BaseGender gender;
