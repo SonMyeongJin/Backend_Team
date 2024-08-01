f2e7ac1b9c7deb63738d92201c177acf5ae8a24d
jungeun <kje2066@naver.com>
2024-08-01 13:27:53 +0900

    feat : 크루등록 dto 작성

---
+++ src/main/java/com/example/likelion12/dto/PostCrewRequest.java
@@ -0,0 +1,16 @@
+package com.example.likelion12.dto;
+
+public class PostCrewRequest {
+    /**
+     * 크루 등록 request dto
+     */
+    private String crewName;
+    private String crewImg;
+    private Long activityRegionId;
+    private Long facilityId;
+    private Long exerciseId;
+    private int totalRecruits;
+    private int crewCost;
+    private String simpleComment;
+    private String comment;
+}
