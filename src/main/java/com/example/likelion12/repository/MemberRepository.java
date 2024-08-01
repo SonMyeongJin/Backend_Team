d91382e188580e3e7bfd56391b0dcf86fa57e4c6
jungeun <kje2066@naver.com>
2024-08-01 14:12:51 +0900

    feat : 필요한 레포지토리들 생성

--- src/main/java/com/example/likelion12/repository/MemberRepository.java
+++ src/main/java/com/example/likelion12/repository/MemberRepository.java
@@ -12,4 +12,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmailAndStatus(String email, BaseStatus status);
     boolean existsByEmailAndStatus(String email, BaseStatus status);
+    Optional<Member> findByIdAndStatus(Long memberId, BaseStatus status);
 }
