f3345bdb2e440277bfc6568926764d84d4544f08
손명진 <sonmyeongjin@sonmyeongjin-ui-MacBookAir.local>
2024-08-01 14:03:28 +0900

    feat : Service 구현

--- src/main/java/com/example/likelion12/repository/CrewRepository.java
+++ src/main/java/com/example/likelion12/repository/CrewRepository.java
@@ -2,10 +2,12 @@ package com.example.likelion12.repository;
 
 import com.example.likelion12.domain.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+@Repository
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 
     // "크루"가 가지고 있는 "크루_멤버"가 많은 순으로 4개 반환 (홈화면 보여주기용)
