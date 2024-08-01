7b9e1dc22e5c7586504658662bad2594fd30dd75
jungeun <kje2066@naver.com>
2024-08-02 03:01:42 +0900

    feat : test

--- src/main/java/com/example/likelion12/controller/TestController.java
+++ src/main/java/com/example/likelion12/controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String likelion(){
-        return "test";
+        return "testㅇㅇㅇㅇ";
     }
 }
