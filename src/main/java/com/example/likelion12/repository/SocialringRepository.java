1385a19c9789a7a81eb07cfb35465cec96d29475
hd0rable <kanghuijin12@gmail.com>
2024-08-01 23:40:09 +0900

    feat : SocialringRepository 작성

--- src/main/java/com/example/likelion12/repository/SocialringRepository.java
+++ src/main/java/com/example/likelion12/repository/SocialringRepository.java
@@ -2,8 +2,12 @@ package com.example.likelion12.repository;
 
 
 import com.example.likelion12.domain.Socialring;
+import com.example.likelion12.domain.base.BaseStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SocialringRepository extends JpaRepository<Socialring, Long> {
-    //Optional<Socialring> findBySocialringIdAndStatus(Long socialringId, BaseStatus status);
+
+    Optional<Socialring> findBySocialringIdAndStatus(Long socialringId, BaseStatus status);
 }
