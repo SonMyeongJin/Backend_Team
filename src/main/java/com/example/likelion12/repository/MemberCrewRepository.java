ba67e638df9c9414a3193b9f6bace8ad71e3b261
jungeun <kje2066@naver.com>
2024-08-02 04:24:08 +0900

    feat : 크루 상세 조회 구현

--- src/main/java/com/example/likelion12/repository/MemberCrewRepository.java
+++ src/main/java/com/example/likelion12/repository/MemberCrewRepository.java
@@ -1,10 +1,16 @@
 package com.example.likelion12.repository;
 
 import com.example.likelion12.domain.MemberCrew;
+import com.example.likelion12.domain.base.BaseStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberCrewRepository extends JpaRepository<MemberCrew, Long> {
 
+    Optional<MemberCrew> findByMember_MemberIdAndCrew_CrewIdAndStatus(Long memberId, Long crewId, BaseStatus status);
+    Optional<List<MemberCrew>> findByCrew_CrewIdAndStatus(Long crewId, BaseStatus status);
 }
