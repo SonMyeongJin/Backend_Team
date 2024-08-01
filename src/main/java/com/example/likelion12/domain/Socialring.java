440fac84e03fce13c0a4fa8ae0e8f9b1d6d7ff4b
hd0rable <kanghuijin12@gmail.com>
2024-08-01 23:39:28 +0900

    feat : Socialring Setter 추가

--- src/main/java/com/example/likelion12/domain/Socialring.java
+++ src/main/java/com/example/likelion12/domain/Socialring.java
@@ -9,6 +9,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Socialring extends BaseTime {
