7ece73773312b7651f3228fc9cbd7e0af6da02e1
jungeun <kje2066@naver.com>
2024-08-02 00:41:05 +0900

    feat : GetCrewDetailResponse dto 작성

---
+++ src/main/java/com/example/likelion12/dto/GetCrewDetailResponse.java
@@ -0,0 +1,41 @@
+package com.example.likelion12.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetCrewDetailResponse {
+    /**
+     * 크루 상세 조회 response dto
+     */
+    private String crewName;
+    private String crewImg;
+    private String activityRegionName;
+    private String exerciseName;
+    private int totalRecruits;
+    private int crewCost;
+    private List<Crews> members;
+    private List<Recommands> recommands;
+
+    @Getter
+    @AllArgsConstructor
+    public static class Crews{
+        private String memberImg;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Recommands{
+        private Long crewId;
+        private String crewName;
+        private String crewImg;
+        private int crewCost;
+        private String activityRegionName;
+        private String exerciseName;
+        private int currentRecruits;
+        private int totalRecruits;
+    }
+}
