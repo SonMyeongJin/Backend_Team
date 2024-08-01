e0dc6686392ebca2d3eafd6a2daabad0d4b56fa0
손명진 <sonmyeongjin@sonmyeongjin-ui-MacBookAir.local>
2024-08-01 14:03:28 +0900

    feat : Service 구현

---
+++ src/main/java/com/example/likelion12/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.likelion12.controller;
+
+import com.example.likelion12.common.response.BaseResponse;
+import com.example.likelion12.dto.HomeResponse;
+import com.example.likelion12.service.HomeService;
+import com.example.likelion12.util.JwtProvider;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class HomeController {
+
+    private JwtProvider jwtProvider;
+    private HomeService homeService;
+
+
+    @GetMapping("/main")
+    public BaseResponse<HomeResponse> getHomeData(@RequestHeader("Authorizatioin") String authorization){
+        Long memberId = jwtProvider.extractIdFromHeader(authorization);
+        return new BaseResponse<>(homeService.getHomeData(memberId));
+    }
+}
--- src/main/java/com/example/likelion12/repository/CrewRepository.java
+++ src/main/java/com/example/likelion12/repository/CrewRepository.java
@@ -2,7 +2,13 @@ package com.example.likelion12.repository;
 
 import com.example.likelion12.domain.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 
+    // "크루"가 가지고 있는 "크루_멤버"가 많은 순으로 4개 반환 (홈화면 보여주기용)
+    @Query("SELECT c FROM Crew c LEFT JOIN c.memberCrewList m GROUP BY c ORDER BY COUNT(m) DESC")
+    List<Crew> findTop4ByMemberCrewListSize();
 }
