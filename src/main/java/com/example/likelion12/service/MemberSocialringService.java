a09633d873f3a97d07c4015d36deb9708a4dcef5
hd0rable <kanghuijin12@gmail.com>
2024-08-01 23:41:02 +0900

    feat : MemberSocialringService의ConfirmCaptainMemberSocialring 작성

--- src/main/java/com/example/likelion12/service/MemberSocialringService.java
+++ src/main/java/com/example/likelion12/service/MemberSocialringService.java
@@ -1,5 +1,6 @@
 package com.example.likelion12.service;
 
+import com.example.likelion12.common.exception.MemberSocialringException;
 import com.example.likelion12.domain.*;
 import com.example.likelion12.domain.base.BaseRole;
 import com.example.likelion12.domain.base.BaseStatus;
@@ -8,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.likelion12.common.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_MEMBERSOCIALRING;
+import static com.example.likelion12.common.response.status.BaseExceptionResponseStatus.CANNOT_MEMBERSOCIALRING_CAPTAIN;
 
 @Slf4j
 @Service
@@ -26,4 +30,17 @@ public class MemberSocialringService {
         MemberSocialring memberSocialring = new MemberSocialring(BaseRole.CAPTAIN,socialring,member,BaseStatus.ACTIVE);
         memberSocialringRepository.save(memberSocialring);
     }
+
+    /**
+     * 소셜링 수정,삭제 시 접근하는 member가  CAPTAIN 인지 확인
+     */
+    @Transactional
+    public void ConfirmCaptainMemberSocialring(MemberSocialring memberSocialring){
+        log.info("[MemberSocialringService.ConfirmCaptainMemberSocialring]");
+
+        if (!BaseRole.CAPTAIN.equals(memberSocialring.getRole())) {
+            throw new MemberSocialringException(CANNOT_MEMBERSOCIALRING_CAPTAIN);
+        }
+    }
+
 }
