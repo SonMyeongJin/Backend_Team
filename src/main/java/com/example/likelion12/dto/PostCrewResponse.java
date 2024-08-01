8bc041bca1afbcb3fb977749e2938f8dbb22abf1
jungeun <kje2066@naver.com>
2024-08-01 13:39:22 +0900

    feat : dto에 어노테이션 추가

--- src/main/java/com/example/likelion12/dto/PostCrewResponse.java
+++ src/main/java/com/example/likelion12/dto/PostCrewResponse.java
@@ -1,5 +1,10 @@
 package com.example.likelion12.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class PostCrewResponse {
     /**
      * 크루 등록 response dto
