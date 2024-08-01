f93dcf4a1dd47ab2d2b09db4cb86238ba0f762d0
hd0rable <kanghuijin12@gmail.com>
2024-08-01 21:48:26 +0900

    feat : CAPTAIN이 아닌 member 들어왔을때 BaseExceptionResponseStatus 작성

--- src/main/java/com/example/likelion12/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/example/likelion12/common/response/status/BaseExceptionResponseStatus.java
@@ -31,7 +31,8 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
      * 6000: member 관련
      */
     CANNOT_FOUND_MEMBER(6000, HttpStatus.BAD_REQUEST.value(), "유저를 찾을 수 없습니다."),
-    ALREADY_EXIST_EMAIL(6001, HttpStatus.BAD_REQUEST.value(), "이미 존재하는 이메일입니다.");
+    ALREADY_EXIST_EMAIL(6001, HttpStatus.BAD_REQUEST.value(), "이미 존재하는 이메일입니다."),
+    CANNOT_MEMBER_CAPTAIN(6002, HttpStatus.BAD_REQUEST.value(), "수정,삭제에 접근할수없는 권한입니다.");
 
 
     private final int code;
