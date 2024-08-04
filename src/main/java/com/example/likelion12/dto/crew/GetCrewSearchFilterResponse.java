3a6d527a5346c183394af513e6dce545c3334b66
hd0rable <kanghuijin12@gmail.com>
2024-08-04 17:07:06 +0900

    fix : response dto 수정

--- src/main/java/com/example/likelion12/dto/crew/GetCrewSearchFilterResponse.java
+++ src/main/java/com/example/likelion12/dto/crew/GetCrewSearchFilterResponse.java
@@ -10,4 +10,12 @@ public class GetCrewSearchFilterResponse {
      * 크루 검색결과 필터링 response dto
      */
     private Long crewId;
+    private String crewName;
+    private String crewImg;
+    private int crewCost;
+    private String activityRegionName;
+    private String exerciseName;
+    private int currentRecruits;
+    private int totalRecruits;
+    private String commentSimple;
 }
