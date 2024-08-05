d39389670556d41e70bcbf774f7058ce5b0ba104
jungeun <kje2066@naver.com>
2024-08-05 19:21:09 +0900

    feat : cors 설정 수정

--- src/main/java/com/example/likelion12/config/SecurityConfig.java
+++ src/main/java/com/example/likelion12/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://example.com"));  // 허용할 도메인 설정
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));  // 허용할 도메인 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));  // 허용할 HTTP 메서드 설정
         configuration.setAllowedHeaders(Arrays.asList("*"));  // 허용할 헤더 설정
         configuration.setAllowCredentials(true);  // 자격 증명 허용 설정
