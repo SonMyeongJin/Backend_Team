d9ba89c3259fbaef726024aba3016096b8501578
jungeun <kje2066@naver.com>
2024-08-06 23:15:44 +0900

    feat : 허용할 도메인 추가

--- src/main/java/com/example/likelion12/config/SecurityConfig.java
+++ src/main/java/com/example/likelion12/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));  // 허용할 도메인 설정
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://frontend-ashen-delta.vercel.app/"));  // 허용할 도메인 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "PUT"));  // 허용할 HTTP 메서드 설정
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));  // 허용할 헤더 설정
         configuration.setAllowCredentials(true);  // 자격 증명 허용 설정
