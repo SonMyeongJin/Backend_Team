42e7c3f28babe17b0f2fb19e74c22305e4a938f4
jungeun <kje2066@naver.com>
2024-08-06 23:09:42 +0900

    feat : 모든 요청 다 받도록 수정

--- src/main/java/com/example/likelion12/config/SecurityConfig.java
+++ src/main/java/com/example/likelion12/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));  // 허용할 도메인 설정
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));  // 허용할 HTTP 메서드 설정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "PUT"));  // 허용할 HTTP 메서드 설정
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));  // 허용할 헤더 설정
         configuration.setAllowCredentials(true);  // 자격 증명 허용 설정
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
